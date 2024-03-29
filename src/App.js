342c2ea9f495959e7bac06346368806061e1e4b8
Siddhant152000 <siddhantdeore15@gmail.com>
2024-03-29 23:37:14 +0530

    Small Change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import NavbarSection from './Navbar'
-// import Homepage from './Homepage'
-// import Footer from './responsiveFooter'
-
-
-
-        
-//         const  App= () => {
-//           return (
-//             <>
-
-//            <div className='container-fluid'>
-//            <NavbarSection/>
-//             <Homepage/>
-//             <Footer/>
-            
-//            </div>
-
-//             </>
-//           )
-//         }
-        
-//         export default App
-
 import React from 'react';
 import NavbarSection from './Navbar';
 import Homepage from './Homepage';
